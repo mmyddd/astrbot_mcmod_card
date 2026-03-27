f614a53ca0e6cb6f92e1d266450aeff76396ad42
mmyddd <mmydddd@gmail.com>
2026-03-27 23:40:04 +0800

    vibe

--- data/data_parse.py
+++ data/data_parse.py
@@ -298,22 +298,24 @@ class ModpackInfoParser(BaseParser):
         return res
 
     def get_description(self):
-        # 优先使用已有的选择器
-        desc_div = self.soup.select_one('div.modpack-description') or self.soup.select_one('div.summary')
-        if desc_div:
-            return {'description': desc_div.get_text(strip=True)}
-
-        # 整合包页面：描述位于 class-text 中的多个 <p> 标签内
         container = self.soup.select_one('div.class-text')
-        if container:
-            # 提取所有 <p> 标签的文本（通常包含主要描述）
-            paragraphs = container.find_all('p')
-            if paragraphs:
-                text = ' '.join(p.get_text(strip=True) for p in paragraphs)
-                if text:
-                    return {'description': text}
-            # 如果没有 <p>，回退到整个容器的文本
+        if not container:
+            return {'description': ''}
+
+        # 查找第一个 <fieldset>（模组列表所在区域）
+        fieldset = container.find('fieldset')
+        if fieldset:
+            # 收集 fieldset 之前的所有元素
+            elements = []
+            for child in container.children:
+                if child == fieldset:
+                    break
+                elements.append(child)
+            # 从收集的元素中提取纯文本
+            from bs4 import BeautifulSoup
+            temp = BeautifulSoup(''.join(str(e) for e in elements), 'lxml')
+            text = temp.get_text(separator=' ', strip=True)
+        else:
+            # 没有 fieldset 时，直接取整个容器的文本（模组页面）
             text = container.get_text(separator=' ', strip=True)
-            if text:
-                return {'description': text}
-        return {'description': ''}
+        return {'description': text}
